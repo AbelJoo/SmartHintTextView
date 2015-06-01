38d1d883049b0a91062d429acfc11ad346e618b3
Abel Joo <abel.joo.1994@gmail.com>
2015-06-01 11:38:38 +0800

    修复sample的一些问题

--- app/src/main/java/com/abeljoo/android/smarthinttextviewtest/MainActivity.java
+++ app/src/main/java/com/abeljoo/android/smarthinttextviewtest/MainActivity.java
@@ -1,9 +1,7 @@
-package com.abeljoo.android.smarthinttextview;
+package com.abeljoo.android.smarthinttextviewtest;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
